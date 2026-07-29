feaa34f6770538a4f0868ef07d4817c6e4ff492e
Vixo-Dev <davidvg1511@gmail.com>
2026-07-29 00:00:22 -0600

    fixv2

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { ProjectDetail } from "./pages/ProjectDetail";
 
 export default function App() {
   return (
-    <Router basename={import.meta.env.BASE_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:id" element={<ProjectDetail />} />
