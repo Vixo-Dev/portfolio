5441cb2695c11f15f588c13beca271e6659f07ef
Vixo-Dev <davidvg1511@gmail.com>
2026-07-28 23:54:50 -0600

    fix

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -4,7 +4,7 @@ import { ProjectDetail } from "./pages/ProjectDetail";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:id" element={<ProjectDetail />} />
